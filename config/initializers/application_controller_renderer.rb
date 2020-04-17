c9af45c625f74914a40e156fa3dd20d8b5f911cb
EC2 Default User <ec2-user@ip-172-31-22-158.ap-southeast-1.compute.internal>
2020-04-17 07:23:02 +0000

    'Railsを学ぼう'完成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '3303feb0936d2aca27837eb603685af5df821e657d8be5b1309bb47600304c716a1738461a153d1aaa1a9729298da74c774898c53a03b65dba3fee9cdef2ac95'
+
